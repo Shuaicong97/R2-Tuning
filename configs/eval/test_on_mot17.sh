3868b42c53debf880b2523a71b90c2ba19272ce4
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2025-11-26 01:27:22 +0100

    add scripts

---
+++ configs/eval/test_on_mot17.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=r2_mot17_test
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --output=/home/atuin/v100dd/v100dd19/sbatch_r2/result-%x-%j.txt
+
+python tools/launch.py configs/qvhighlights/r2_tuning_mot17.py --checkpoint /home/atuin/v100dd/v100dd19/R2-Tuning/work_dirs/r2_tuning_mot17/epoch_30.pth --eval --work_dir /home/atuin/v100dd/v100dd19/R2-Tuning/outputs/mot17
