cff8fa2a300207912963e36a5f4c73bcea4768cd
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2025-11-26 01:06:13 +0100

    update runtime to get best

--- configs/_base_/runtime.py
+++ configs/_base_/runtime.py
@@ -2,6 +2,9 @@
 hooks = dict(
     type='EvalHook',
     high_keys=[
-        'MR-full-mAP', 'MR-full-mIoU', 'HL-min-VeryGood-mAP', 'MR-full-R1@0.1',
-        'MR-full-R1@0.3', 'MR-full-R1@0.5', 'mAP'
+        'MR-full-mAP', 'MR-full-mIoU', 'HL-min-VeryGood-mAP',
+        'MR-full-mAP@0.1', 'MR-full-mAP@0.3', 'MR-full-mAP@0.5',
+        'MR-full-R1@0.1', 'MR-full-R1@0.3', 'MR-full-R1@0.5',
+        'MR-full-R5@0.1', 'MR-full-R5@0.3', 'MR-full-R5@0.5',
+        'MR-full-R10@0.1', 'MR-full-R10@0.3', 'MR-full-R10@0.5', 'mAP'
     ])
