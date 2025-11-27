abc684f663cce85f1c4a955104454d9856bb0673
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2025-11-27 15:34:03 +0100

    add script

---
+++ scripts/mot20_a40.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=train_mot20_r2_a40
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:a40:1
+#SBATCH --output=/home/atuin/v100dd/v100dd19/sbatch_r2/result-%x-%j.txt
+
+PYTHONPATH=$PYTHONPATH:. python tools/launch.py configs/qvhighlights/r2_tuning_mot20.py \
+--work_dir /home/atuin/v100dd/v100dd19/R2-Tuning/best_runtime/r2_tuning_mot20_a40
